33c940f5695b10b796fcae4945933ae57231b855
Omar Ahmed <cresssoomar@gmail.com>
2026-09-16 15:31:56 +0300

    Theoretical Q1

--- Smart Delivery Management System/Program.cs
+++ Smart Delivery Management System/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+
+#region practical Q1
+/*
+ * a)  What happens when a DeliveryAddress variable is copied into another variable 
+  and the copy is modified? 
+b)  What happens when a Customer variable is copied into another variable and one variable modifies 
+the object
+public struct DeliveryAddress
+{
+public string City;
+public string Street;
+}
+public class Customer
+{
+public string Name;
+}*/
+#endregion
+/*response
+a) DeliveryAddress is a struct (Value Type). When it is copied, a completely independent,
+new copy of the data is allocated on the Stack.
+Result: Modifying the copy will NOT affect the original variable.
+--------------------------------------------------------------
+b) Customer is a class (Reference Type): When it is copied, only the memory reference is copied, while the actual object 
+remains in the Heap. Both variables now point to the exact same object in memory.
+Result: Modifying the object through either variable WILL affect both variables.
+*/
